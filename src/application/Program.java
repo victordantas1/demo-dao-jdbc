335b21e29557817d2528aadaff60f9234af92007
Victor Dantas <victorr.dantas@gmail.com>
2023-09-01 16:04:35 -0400

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         Seller seller = new Seller(21,"Victor Dantas", "vict@gmail.com", new Date(), 3000.0, obj);
 
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,36 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+
+
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
